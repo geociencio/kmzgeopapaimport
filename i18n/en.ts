8b3dbdb09f6eed9020a8885991d5847e2ab8aef9
juanb <juanbernales@gmail.com>
2025-07-27 09:31:57 -0400

    Se agregaron archivos para las traducciones mas importantes

---
+++ i18n/en.ts
@@ -0,0 +1,201 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>kmz_geopapaimport</name>
+    <message>
+        <location filename="../kmz_geopapaimport.py" line="181"/>
+        <source>&amp;Impport kmz files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport.py" line="168"/>
+        <source>Import kmz from geopaparazzi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport.py" line="214"/>
+        <source>Could not process JSON file: {e}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport.py" line="222"/>
+        <source>kml.kml not found in the KMZ archive.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport.py" line="254"/>
+        <source>CRS for transformation is not defined.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>kmz_geopapaimportDialog</name>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="52"/>
+        <source>Select KMZ file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="52"/>
+        <source>KMZ files (*.kmz *.KMZ)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="65"/>
+        <source>Select JSON file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="65"/>
+        <source>JSON files (*.json *.JSON)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="173"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="82"/>
+        <source>Could not read or parse JSON file: {error}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="91"/>
+        <source>Select destination folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="116"/>
+        <source>Invalid CRS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="116"/>
+        <source>The active layer has an invalid CRS.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="122"/>
+        <source>No active layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="122"/>
+        <source>There is no active layer in QGIS.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="153"/>
+        <source>All input fields are required.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="156"/>
+        <source>Processing {form_name}...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="169"/>
+        <source>
+Success! File saved to:
+{output_path}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="172"/>
+        <source>
+ERROR: {e}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog.py" line="187"/>
+        <source>Help file not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>kmz_geopapaimportDialogBase</name>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="14"/>
+        <source>Impport kmz files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="26"/>
+        <source>KMZ to CSV from Geopaparazzi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="39"/>
+        <source>(Based on JSON format)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="78"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="131"/>
+        <source>Add point transformation?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="149"/>
+        <source>Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="162"/>
+        <source>Active Layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="175"/>
+        <source>Get CRS from:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="188"/>
+        <source>(This action add xtrans and ytrans fields)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="212"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="225"/>
+        <source>Reset Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="238"/>
+        <source>Process Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="274"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="287"/>
+        <source>Output folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="300"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmz_geopapaimport_dialog_base.ui" line="313"/>
+        <source>exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
